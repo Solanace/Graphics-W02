c70fc0ec6643284e1b95ec359cd529f122d7bb3e
Penn <penn.z.wu@gmail.com>
2018-02-08 23:21:15 -0500

    Finished

--- draw.c
+++ draw.c
@@ -14,12 +14,109 @@ int find_intercept(int x0, int y0, int x1, int y1) {
 
 //Insert your line algorithm here
 void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
-
-// Octant I
-int A = y1 - y0;
-int B = x0 - x1;
-int C = find_intercept(x0, y0, x1, y1) * (x1 - x0);
-//int x = x0, y = y0;
-printf("%d\n", A + B + C);
-
+	if (x0 == x1) { // Vertical line
+		if (y0 < y1) {
+			while (y0 <= y1) {
+				plot(s, c, x0, y0);
+				y0 ++;
+			}
+		}
+		else draw_line(x1, y1, x0, y0, s, c);
+		return;
+	}
+	if (y0 == y1) { // Horizontal line
+		if (x0 < x1) {
+			while (x0 <= x1) {
+				plot(s, c, x0, y0);
+				x0 ++;
+			}
+		}
+		else draw_line(x1, y1, x0, y0, s, c);
+		return;
+	}
+	int A = y1 - y0;
+	int B = x0 - x1;
+	int x = x0, y = y0;
+	int d;
+	double m = (y1 - y0) * 1.0 / (x1 - x0);
+	if (m == 1) {
+		if (x0 < x1) {
+			while (x0 <= x1) {
+				plot(s, c, x0, y0);
+				x0 ++;
+				y0 ++;
+			}
+		}
+		else draw_line(x1, y1, x0, y0, s, c);
+	}
+	else if (m == -1) {
+		if (x0 < x1) {
+			while (x0 <= x1) {
+				plot(s, c, x0, y0);
+				x0 ++;
+				y0 --;
+			}
+		}
+		else draw_line(x1, y1, x0, y0, s, c);
+	}
+	else if (m > 0 && m < 1) {
+		if (x0 < x1) { // Octant I
+			d = 2 * A + B;
+			while (x <= x1) {
+				plot(s, c, x, y);
+				if (d > 0) {
+					y ++;
+					d += 2 * B;
+				}
+				x ++;
+				d += 2 * A;
+			}
+		}
+		else draw_line(x1, y1, x0, y0, s, c); // Octant V
+	}
+	else if (m > 1) {
+		if (x0 < x1) { // Octant II
+			d = A + 2 * B;
+			while (x <= x1) {
+				plot(s, c, x, y);
+				if (d < 0) {
+					x ++;
+					d += 2 * A;
+				}
+				y ++;
+				d += 2 * B;
+			}
+		}
+		else draw_line(x1, y1, x0, y0, s, c); // Octant VI
+	}
+	else if (m < 0 && m > -1) {
+		if (x0 < x1) { // Octant VIII
+			d = 2 * A - B;
+			while (x <= x1) {
+				plot(s, c, x, y);
+				if (d < 0) {
+					y --;
+					d -= 2 * B;
+				}
+				x ++;
+				d += 2 * A;
+			}
+		}
+		else draw_line(x1, y1, x0, y0, s, c); // Octant IV
+	}
+	else if (m < -1) {
+		if (x0 < x1) { // Octant VII
+			d = A - 2 * B;
+			while (x <= x1) {
+				plot(s, c, x, y);
+				if (d > 0) {
+					x ++;
+					d += 2 * A;
+				}
+				y --;
+				d -= 2 * B;
+			}
+		}
+		else draw_line(x1, y1, x0, y0, s, c); // Octant III
+	}
 }
--- main.c
+++ main.c
@@ -7,17 +7,39 @@
 #include "draw.h"
 
 int main() {
+	screen s;
+	color c;
 
-  screen s;
-  color c;
+	c.red = 255;
+	c.green = 165;
+	c.blue = 0;
 
-  c.red = 0;
-  c.green = MAX_COLOR;
-  c.blue = 0;
-
-  clear_screen(s);
-
-
-  display(s);
-  save_extension(s, "lines.png");
+	clear_screen(s);
+	// P (undefined, 0)
+	draw_line(75, 150, 75, 350, s, c); // Undefined
+	draw_line(75, 350, 175, 350, s, c); // 0
+	draw_line(175, 350, 175, 280, s, c); // Undefined
+	draw_line(175, 280, 75, 280, s, c); // 0
+	// W (II/VI, III/VII)
+	draw_line(225, 350, 275, 150, s, c); // VII
+	draw_line(275, 150, 325, 350, s, c); // II
+	draw_line(375, 150, 325, 350, s, c); // III
+	draw_line(425, 350, 375, 150, s, c); // VI
+	// Diamond (1, -1)
+	c.blue = 200;
+	draw_line(100, 315, 125, 340, s, c); // 1
+	draw_line(125, 340, 150, 315, s, c); // -1
+	draw_line(125, 290, 100, 315, s, c); // -1
+	draw_line(150, 315, 125, 290, s, c); // 1
+	// Triangle (I/V, IV/VIII)
+	c.red = 100;
+	c.green = 255;
+	draw_line(225, 150, 425, 150, s, c); // 0
+	draw_line(225, 150, 275, 125, s, c); // VIII
+	draw_line(325, 100, 275, 125, s, c); // IV
+	draw_line(325, 100, 375, 125, s, c); // I
+	draw_line(425, 150, 375, 125, s, c); // V	
+	
+	display(s);
+	save_extension(s, "lines.png");
 }
